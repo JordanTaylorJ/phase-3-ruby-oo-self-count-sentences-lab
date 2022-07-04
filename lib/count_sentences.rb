dc37fa9fd9347f9a3667a0674fef0ef6c07548aa
JordanJoseph <jordant.joseph@outlook.com>
2022-07-03 18:58:23 -0600

    commit tree f8bb67d09af992c14dae4dbce074a4420e07c65b

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    end_with? "."
   end
 
   def question?
-
+    self.end_with? "?"
   end
 
   def exclamation?
-
+    self.end_with? "!"
   end
 
   def count_sentences
-
+    self.split(/(?:\.|\?|\!)/&&/(?<=\w)/).count
+    #binding.pry
   end
+  
 end
+
+#(?<!\w) 
+#(?:\.|\?|\!)(?= [^a-z]|$)
+
+# MY  ((?<!\w) + /\.|\?|\!/)
+
+#"This, well, is a sentence. This is too!! And so is this, I think? Woo..."
